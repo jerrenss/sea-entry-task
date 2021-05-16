af1dff4babdd4eeb4b41390ecaca9215173ed369
Wei Xiang Gerren Seow <seowgwx@sea.com>
2021-05-16 20:52:55 +0800

    feat: dockerised application

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,7 @@
 const axios = require('axios')
-export const DEV_API_URL: string = process.env.NEXT_PUBLIC_DEV_API_URL
 
 export const apiClient = axios.create({
-  baseURL: DEV_API_URL,
+  baseURL: '/api',
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
